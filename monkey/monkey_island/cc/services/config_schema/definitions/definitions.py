1941e99be17ef1352678a6c3ef326d486f507887
VakarisZ <vakarisz@yahoo.com>
2020-11-18 09:19:07 +0200

    Extracted definitions from config schema

---
+++ monkey/monkey_island/cc/services/config_schema/definitions/definitions.py
@@ -0,0 +1,12 @@
+from monkey_island.cc.services.config_schema.definitions.exploiter_classes import EXPLOITER_CLASSES
+from monkey_island.cc.services.config_schema.definitions.finger_classes import FINGER_CLASSES
+from monkey_island.cc.services.config_schema.definitions.post_breach_actions import POST_BREACH_ACTIONS
+from monkey_island.cc.services.config_schema.definitions.system_info_collector_classes import \
+    SYSTEM_INFO_COLLECTOR_CLASSES
+
+DEFINITIONS = {
+    "exploiter_classes": EXPLOITER_CLASSES,
+    "system_info_collector_classes": SYSTEM_INFO_COLLECTOR_CLASSES,
+    "post_breach_actions": POST_BREACH_ACTIONS,
+    "finger_classes": FINGER_CLASSES
+}
